4cf13807daf3a9e03fd009746ecdecf8cb97ad98
mklemann <mklemann@live.com>
2019-07-27 20:40:07 -0300

    adding button component

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './Calculator.css'
 
+import Button from '../components/Button'
+
 export default class Calculator extends Component {
     render() {
         return (
             <div className='calculator'>
-            
+                <Button value='1'/>
             </div>
         )
     }
