dd45ca1d0613d1fb69ba33f921f1b51045973923
mklemann <mklemann@live.com>
2019-07-27 19:34:17 -0300

    style on body

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+import Calculator from './main/Calculator'
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <h1>mklemann</h1>
+    <Calculator />
+  </div>,
   document.getElementById('root')
 );
